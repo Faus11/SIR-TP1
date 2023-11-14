7bffb25d163d11ef81b085b6a3768a611513b3b1
Tafonso91 <t.veloso.afonso@ipvc.pt>
2023-11-14 23:13:29 +0000

    pequenas alterações no html e perguntas

---
+++ forms.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.question').forEach(item => {
+    item.addEventListener('click', () => {
+        const answer = item.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
