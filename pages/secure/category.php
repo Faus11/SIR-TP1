3198266ebf0952354bcd0698c588e028aa5dca1d
Tafonso91 <t.veloso.afonso@ipvc.pt>
2024-02-05 16:35:46 +0000

    primeiro modal feito

--- pages/secure/category.php
+++ pages/secure/category.php
@@ -84,10 +84,35 @@ if ($contentByCategory && isset($contentByCategory[$category_id])) {
               <div class="d-flex justify-content-between mt-3">
                 <a href="contentId.php?id=<?= $contentId ?>" class="btn btn-warning btn-block">View</a>
                 <form action="../../controllers/content/content.php" method="post">
-                  <input type="hidden" name="id" value="<?= $contentId ?>">
-                  <button type="submit" class="btn btn-danger btn-block" name="content" value="delete">Delete</button>
+                    <input type="hidden" name="id" value="<?= $contentId ?>">
+                    <button type="button" class="btn btn-danger btn-block" data-toggle="modal" data-target="#deleteModal<?= $contentId ?>">Delete</button>
                 </form>
                 <a href="updateContent.php?id=<?= $contentId ?>" class="btn btn-info btn-block">Edit</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Modal de Confirmação para Delete -->
+        <div class="modal fade" id="deleteModal<?= $contentId ?>" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel<?= $contentId ?>" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content"  >
+              <div class="modal-header">
+                <h5 class="modal-title " id="deleteModalLabel <?= $contentId ?>" style="color: black;">Confirm Delete </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true"  style="color: black;">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body" style="color: black;">
+
+                Are you sure you want to delete this content?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <form action="../../controllers/content/content.php" method="post">
+                    <input type="hidden" name="id" value="<?= $contentId ?>">
+                    <button type="submit" class="btn btn-danger" name="content" value="delete">Delete</button>
+                </form>
               </div>
             </div>
           </div>
@@ -110,3 +135,11 @@ if ($contentByCategory && isset($contentByCategory[$category_id])) {
   }
 }
 ?>
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
